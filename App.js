1fbadd0b7417fb258d268b5cfd071e21d63e7072
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-05-07 23:13:08 +0900

    배경이미지, 텍스트 추가

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, Text, View, StatusBar, ImageBackground,
+  Dimensions
+} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import {
+  responsiveHeight,
+  responsiveFontSize,
+  responsiveWidth
+} from 'react-native-responsive-dimensions';
+
+//상태바
+StatusBar.setBackgroundColor("transparent");
+StatusBar.setTranslucent(true);
+StatusBar.setBarStyle("white-content");
 
 export default class App extends React.Component {
   componentDidMount() {
@@ -8,15 +22,32 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View>
-        <View>
-
+      <ImageBackground
+        style={styles.background_img}
+        source={require("./src/images/main-background.jpg")}
+        resizeMode="stretch"
+        >
+        <View style={styles.container}>
+          <Text style={styles.main_title}>AIR</Text>
+          <Text>HELPER</Text>
         </View>
-        <Text>
-          Now Loading...
-        </Text>
-      </View>
+      </ImageBackground>
     );
   }
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: getStatusBarHeight(),
+    height: responsiveHeight(100),
+  },
+  background_img: {
+    width: responsiveWidth(100),
+    height: responsiveHeight(100),
+  },
+  main_title: {
+    fontSize: responsiveFontSize(10),
+    color: 'white',
+  }
+});
